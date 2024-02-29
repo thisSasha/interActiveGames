cf6c70894f7c07c26506a2b3f50d6bf6437d8e4b
Саня <trhfjfn@gmail.com>
2024-02-29 19:05:43 +0300

    Beta 1.0

---
+++ js/startPage.js
@@ -0,0 +1,18 @@
+document.querySelector('button').onclick = function () {
+    let components = ['difficulty', 'name', 'divide', 'repeat', 'freeOrder'];
+    let abcd = true;
+    for (let i = 0; i < components.length; i++) {
+        const el = components[i];
+        if (document.getElementById(el).validity.valid) {
+            if (document.querySelector('#' + el).value == 'on') {
+                localStorage.setItem(el, document.querySelector('#' + el).checked);
+            } else {
+                localStorage.setItem(el, document.querySelector('#' + el).value);
+            };
+        } else {
+            abcd = false;
+            alert('Вы не все заполнили правильно. Примечание: число должно быть от 2 до 9');
+        };
+    };
+    if (abcd) window.location.replace(/*'https://thissasha.github.io/interActiveGames/post.html'*/'/game.html');
+};
